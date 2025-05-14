1bad669a5d851977bd102232f11e46415aef3df9
Szymon <szychoruzy@gmail.com>
2025-05-14 12:11:44 +0200

    poprawki

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,18 @@
+<<<<<<< HEAD
 import './bootstrap';
 //
+=======
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { InertiaProgress } from '@inertiajs/progress';
+
+// Inicjalizacja aplikacji
+createInertiaApp({
+    resolve: name => import(`./Pages/${name}.vue`),  // dynamiczne importowanie komponentów
+    setup({ el, App, props }) {
+        createApp({ render: () => h(App, props) }).mount(el);
+    },
+});
+
+InertiaProgress.init();
+>>>>>>> 8f0c9ba (poprawki)
